87ae590b931df9e522195b1fecd5f18b20815dc0
avanago <andre.baroni2@gmail.com>
2017-12-08 11:13:17 +0100

    Update coinbaseLinker.py
    Update methods, introduce dependencies, work in sandbox mode

--- interface/coinbaseLinker.py
+++ interface/coinbaseLinker.py
@@ -1,24 +1,30 @@
+import json
+import requests
 
+class coinbaseAccount():
+    def __init__(self, auth, coinbase_account_id, prodTrader=False):
+        self.coinbase_account_id = coinbase_account_id
+        self.auth = auth
+        if prodTrader:
+            self._url = 'https://api.gdax.com'
+        else:
+            self._url = 'https://api-public.sandbox.gdax.com'
 
-class coinbaseAccount(self)
-  def __init__(self):
+    def depositFromCoinbase(self, amount = 0, currency=""):
+        payload = {
+            "amount": amount,
+            "currency": currency,
+            "coinbase_account_id": self.coinbase_account_id
+        }
+        r = requests.post(self._url + "/deposits/coinbase-account", data=json.dumps(payload), auth=self.auth, timeout=30)
+        return r.json()
 
-  def coinbase_deposit(self, amount="", currency="", coinbase_account_id=""):
-    payload = {
-        "amount": amount,
-        "currency": currency,
-        "coinbase_account_id": coinbase_account_id
-    }
-    r = requests.post(self.url + "/deposits/coinbase-account", data=json.dumps(payload), auth=self.auth, timeout=30)
-    # r.raise_for_status()
-    return r.json()
-
-  def coinbase_withdraw(self, amount="", currency="", coinbase_account_id=""):
-    payload = {
-        "amount": amount,
-        "currency": currency,
-        "coinbase_account_id": coinbase_account_id
-    }
-    r = requests.post(self.url + "/withdrawals/coinbase-account", data=json.dumps(payload), auth=self.auth, timeout=30)
-    # r.raise_for_status()
-    return r.json()
+    def withdrawToCoinbase(self, amount=0, currency=""):
+        payload = {
+            "amount": amount,
+            "currency": currency,
+            "coinbase_account_id": self.coinbase_account_id
+        }
+        r = requests.post(self._url + "/withdrawals/coinbase-account", data=json.dumps(payload), auth=self.auth, timeout=30)
+        return r.json()
+      
