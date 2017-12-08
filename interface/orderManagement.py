662ae5c75bad9daf9421275ff0642b247da2736e
avanago <andre.baroni2@gmail.com>
2017-12-08 16:39:15 +0100

    Update orderManagement.py
    Minor revision after tests in sandbox configuration

--- interface/orderManagement.py
+++ interface/orderManagement.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-class orderManager(self):
+class orderManager():
     def __init__(self, auth, prodTrader=False):
         self.auth = auth
         if prodTrader:
@@ -12,7 +12,7 @@ class orderManager(self):
     def renewAuth(self,auth):
         self.auth = auth
     
-    def buy(self, **kwargs):
+    def buy(self, kwargs):
         kwargs["side"] = "buy"
         r = requests.post(self._url + '/orders',
                           data=json.dumps(kwargs),
@@ -20,7 +20,7 @@ class orderManager(self):
                           timeout=30)
         return r.json()
 
-    def sell(self, **kwargs):
+    def sell(self, kwargs):
         kwargs["side"] = "sell"
         r = requests.post(self._url + '/orders',
                           data=json.dumps(kwargs),
@@ -33,20 +33,15 @@ class orderManager(self):
         return r.json()
 
     def cancel_all(self, product_id=''):
-        _url = self._url + '/orders/'
-        if product_id:
-            _url += "?product_id={}&".format(str(product_id))
-        r = requests.delete(_url, auth=self.auth, timeout=30)
+        url = self._url + '/orders'
+        r = requests.delete(url, auth=self.auth, timeout=30)
         return r.json()
-
+    
     def get_order(self, order_id):
         r = requests.get(self._url + '/orders/' + order_id, auth=self.auth, timeout=30)
         return r.json()
 
     def get_orders(self, product_id=''):
-        result = []
-        _url = self._url + '/orders/'
-        if product_id:
-            _url += "?product_id={}&".format(product_id)
-        r = requests.get(_url, auth=self.auth, timeout=30)
-        return result
+        url = self._url + '/orders?status=all'
+        r = requests.get(url, auth=self.auth, timeout=30)
+        return r.json()
